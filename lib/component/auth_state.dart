595a4aabec5fbcc29e71cf8ee58ccae09f2e84ea
Ahmad Huzaifi Bin Ahmad Azahri <ujaiahmad@gmail.com>
2022-10-22 16:22:23 +0800

    pages folder
    add accout_page.dart and login_page.dart

--- lib/component/auth_state.dart
+++ lib/component/auth_state.dart
@@ -1,7 +1,13 @@
+// In order to handle deep links for Android and iOS,
+//let's create a class that will do just that. supabase_flutter
+//plugin comes with SupabaseAuthState class where we can inherit
+//from to react to various deep link events.
 import 'package:flutter/material.dart';
 import 'package:supabase/supabase.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:supabase_quickstart/utils/constants.dart';
+//import 'package:supabase_quickstart/utils/constants.dart';
+import 'package:testfyp/utils/constants.dart';
+//import 'C:\flutter\flutter clone repositories\frontend\lib\utils\constants.dart';
 
 class AuthState<T extends StatefulWidget> extends SupabaseAuthState<T> {
   @override
