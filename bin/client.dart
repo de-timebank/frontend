25b3310a37e9df911568902a619f1037991e639a
Ahmad Huzaifi Bin Ahmad Azahri <ujaiahmad@gmail.com>
2022-11-01 13:52:27 +0800

    dapat get request rating

--- bin/client.dart
+++ bin/client.dart
@@ -1,31 +1,35 @@
-// import 'package:flutter/foundation.dart';
-// import 'package:grpc/grpc.dart';
-// import 'package:testfyp/generated/auth_proto/auth.pbgrpc.dart';
+//import 'package:flutter/cupertino.dart';
+import 'package:grpc/grpc.dart';
+import 'package:testfyp/generated/rating/collection/rating.pbgrpc.dart';
+//import 'package:testfyp/generated/service_request/collection/service-request.pbgrpc.dart';
 
-// class Client {
-//   //ClientChannel? channel;
-//   late AuthClient stub;
+void main() async {
+  final channel = ClientChannel('127.0.0.1',
+      port: 8080,
+      options: ChannelOptions(credentials: ChannelCredentials.insecure()));
 
-//   Future<void> main() async {
-//     final channel = ClientChannel('127.0.0.1', //ammar bagi
-//         port: 8080,
-//         options:
-//             const ChannelOptions(credentials: ChannelCredentials.insecure()));
-//     // var client = Client(
-//     //     //client.main();
-//     //     );
-//     stub = AuthClient(channel,
-//         options: CallOptions(timeout: const Duration(seconds: 30)));
+  final stub = RatingClient(channel,
+      options: CallOptions(timeout: Duration(seconds: 30)));
 
-//     try {
-//       await SignIn();
-//       await SignUp();
-//     } catch (e) {
-//       print('Caught error: $e');
-//     }
-//   }
+  // final stub = ServiceRequestClient(channel,
+  //     options: CallOptions(timeout: Duration(seconds: 30)));
 
-//   // var response;
-//   // bool executionInProgress = true;
+  // var response = await stub.getById(
+  //     GetById_Request()..requestId = '9b5f1dca-ea47-4774-867c-8102daef586c');
 
-// }
+  var response = await stub.get(Get_Request()
+    ..key = 'id'
+    ..value = '416056b5-e518-4e1b-8770-8d9ac1f58868');
+  //value: '5', key: '416056b5-e518-4e1b-8770-8d9ac1f58868'
+  // rating: Create_NewRatingData(
+  //         author: '185552ab-8fbc-4cc0-a9e3-0c660bc4acd6',
+  //         comment: 'I love you',
+  //         value: 4,
+  //         requestId: '19bbe583-1cf1-4b88-81a6-5fcb1ff5010c')
+
+  //var response = await stub.createForProvider(Create_Request());
+
+  print('The response is ' + response.toString());
+
+  await channel.shutdown();
+}
