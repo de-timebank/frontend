206af73a40d6b91c684dde3c4108ea0f3b1d9e10
Ahmad Huzaifi Bin Ahmad Azahri <ujaiahmad@gmail.com>
2022-11-05 19:12:51 +0800

    change key

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,9 @@ Future<void> main() async {
   const ujaiUrl = 'https://qcoqbidwruiakpykidlf.supabase.co';
   const ujaiKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjb3FiaWR3cnVpYWtweWtpZGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYzNDEzODYsImV4cCI6MTk4MTkxNzM4Nn0.ol5POkel6KTRNrDk7Z_yUvYhzJ7_SXbY7Yt94Wvzi-Q';
-  const ammarUrl = 'https://quepskrrpovzwydvfezs.supabase.co/rest/v1';
+  const ammarUrl = 'https://quepskrrpovzwydvfezs.supabase.co';
   const ammarKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1ZXBza3JycG92end5ZHZmZXpzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzEyNTU3OSwiZXhwIjoxOTgyNzAxNTc5fQ.VwMvERKSiR_7fS_H3ROWBrTCnYkWrBtkPDRj8s8Ma_E';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1ZXBza3JycG92end5ZHZmZXpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcxMjU1NzksImV4cCI6MTk4MjcwMTU3OX0.DCX9Qw3GKYdg-Ew96DHU-4wCIqdFm-AS7S63AAx8wFQ';
 
   await Supabase.initialize(url: ammarUrl, anonKey: ammarKey);
   runApp(MyApp());
